83e91879fe4aff35bfcc09c3aeb01752fcf37ed9
Fredrik Gerdin Börjesson <frebo147@student.liu.se>
2018-11-01 00:38:24 +0100

    I have no idea what I'm doing. Fixing tomorrow.

--- analysis.R
+++ analysis.R
@@ -3,13 +3,18 @@ fetchData <- function(fileName) {
   return(dataSet)
 }
 
-printNames <- function(setName) {
+printAttributes <- function(setName) {
   names(setName)
 }
 
 
-normalizeData<- function() {
-  
+normalizeData <- function(dataSet) {
+  t(apply(dataSet, 1, function(x)(x-min(x))/(max(x)-min(x))))
+  #t(apply(mydata, 1, function(x)(x-min(x))/(max(x)-min(x))))
+ # https://stackoverflow.com/questions/20046257/normalize-rows-of-a-matrix-within-range-0-and-1
+}
+
+getChange <- function(dataSet1, dataSet2) {
   
   
 }
--- calls.R
+++ calls.R
@@ -5,10 +5,27 @@ library(maps)
 library(ggmap)
 library(lubridate)
 library(gridExtra)
+library(data.table)
 
+#Load the built functions from separate files
 source("analysis.R")
 
+#Temp method of loading data
 dataSet <- fetchData("Mockdata1.csv")
 
-#Print the names on the attributes in the table specified by the name
-printNames(dataSet)
+#my_mongo = mongo(collection = "users", db = "tempDB")
+#my_mongo$insert(dataSet)
+
+#Print the names of the attributes in the table specified by the input parameter
+printAttributes(dataSet)
+
+gdprTRUE <- dataSet[GDPR_consent == TRUE]
+
+#normalizeData(dataSet)
+#(dataSet)[ , .(mean_r_news = mean(R_news), mean_r_pol = mean(R_sports))]
+means <- dataSet[, lapply(.SD, mean)]
+
+
+#t(apply(dataSet, 10, function(x)(x-min(x))/(max(x)-min(x))))
+apply(dataSet, 8:12, min)
+#head(dataSet)
