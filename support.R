41f6d873b5a8e97efb0c2d9eae7da16ee95f01f6
Fredrik Gerdin Börjesson <frebo147@student.liu.se>
2018-11-06 23:03:25 +0100

    Added several interface items. Trying to get icons to show.

--- support.R
+++ support.R
@@ -5,17 +5,17 @@ SaveData <- function(data.Name, separator, output.File.Name) {
   fwrite(data.Name, file = output.File.Name, sep = separator)
 }
 
-DBConnect <- function() {
-  user <- readline(prompt = "Enter username: ")
-  pass <- readline(prompt = "Enter password: ")
+DBConnect <- function(user, pass) {
   URI <- paste0("mongodb://", user, ":", pass, "@pumi-4-1.tddc88-2018.ida.liu.se:27017/newz")
   mango = mongo(url = URI)
   #url format: "mongodb://[username:password@]host1[:port1][,host2[:port2],...[/[database][?options]]"
+  print("Connected")
   return(mango)
 }
 
 DBDisconnect <- function(db.env) {
   db.env$disconnect()
+  print("Discon")
 }
 
 DBPrintAll <- function(db.env) {
