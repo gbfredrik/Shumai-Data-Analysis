aa66e10e66f2a75a74b0629f7043701d4af88555
Fredrik Gerdin Börjesson <frebo147@student.liu.se>
2018-12-07 23:03:52 +0100

    Fixed the remaining bugs. Still some warning messages, but they're handled without any crashes or issues. No further work to be done.
    Adios!

--- analysis.R
+++ analysis.R
@@ -1,14 +1,14 @@
 CountViewsByDates <- function(data.set, dates) { # This function counts the number of views between the two dates given by the parameter "dates". Returns the complete user table.
   names <- c("animals", "auto", "people", "fun", "creation", "school", "videogames", "kids", "lifestyle", "shortfilms", "music", "news", "sport", "tech", "travel", "tv", "webcam")
-  test.set <- data.frame(matrix(ncol = 17, nrow = nrow(data.set)))
-  colnames(test.set) <- names
-  rownames(test.set) <- data.set$user_name
+  new.set <- data.frame(matrix(ncol = 17, nrow = nrow(data.set)))
+  colnames(new.set) <- names
+  rownames(new.set) <- data.set$user_name
 
   for (i in 1:nrow(data.set)) {
-    test.set[i,] <- lapply(1:17, function(X) length(which(data.set$category_views[[X]][[i]] >= dates[1] & 
+    new.set[i,] <- lapply(1:17, function(X) length(which(data.set$category_views[[X]][[i]] >= dates[1] & 
                                                             data.set$category_views[[X]][[i]] <= dates[2])))
   }
-  return(test.set)
+  return(new.set)
 }
 
 NormalizeData <- function(data.set) { # Gives each user a rating of their interest for each category, between 0 and 1. 
@@ -22,7 +22,9 @@ FilterCategories <- function(data.set, filter.list) { # Filters users based on t
   
   index <- seq(1, 33, 2)
   for (i in 1:17) {
-    new.set <- new.set[(new.set[i] >= filter.list[index[i]] & new.set[i] <= filter.list[index[i]+1]), ]
+    if (nrow(new.set) != 0) {
+      new.set <- new.set[(new.set[i] >= filter.list[index[i]] & new.set[i] <= filter.list[index[i]+1]), ]
+    }
   }
   return(new.set)
 }
@@ -37,4 +39,16 @@ GetWorstCategory <- function(data.set, limit) { # Returns the category with the 
   new.set <- as.data.frame(data.set)
   counted.favs <- sapply(1:17, function(X) length(which(new.set[, X] <= limit )))
   return(colnames(new.set)[which(counted.favs==max(counted.favs))])
+}
+
+FilterViewsAFterFilters <- function(original.set, filtered.set) {
+  new.original.set <- as.data.frame(original.set)
+  new.filtered.set <- as.data.frame(filtered.set)
+  
+  if (nrow(new.original.set) != 0 && nrow(new.filtered.set) != 0) {
+    new.original.set <- new.original.set[rownames(new.original.set) %in% rownames(new.filtered.set), ]
+    return(new.original.set)
+  } else {
+    return(0)
+  }
 }
